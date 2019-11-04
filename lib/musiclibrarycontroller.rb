ceb97cd6e7add95b5587bc9480a5705c416b975f
alycia hall <amhall25@gmail.com>
2019-11-04 02:19:01 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -44,14 +44,14 @@ class MusicLibraryController
         puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
       end
   end
-   #this gives you an array of song instances sorted by name alphabetically
-    songs_sorted_by_name = Song.all.sort_by do |song|
-      song.name
-    end
-    songs_sorted_by_name.each.with_index(1) do |song,index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    end
-  end
+  # #this gives you an array of song instances sorted by name alphabetically
+  #   songs_sorted_by_name = Song.all.sort_by do |song|
+  #     song.name
+  #   end
+  #   songs_sorted_by_name.each.with_index(1) do |song,index|
+  #     puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  #   end
+  # end
 
   def list_artists
     #prints all artists in the music library in a numbered list (alphabetized by artist name
